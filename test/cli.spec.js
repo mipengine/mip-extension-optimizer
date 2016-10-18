eb3ccbe2fd40cdec328fdab48cfcc24708b87386
erik <erik168@163.com>
2016-10-18 21:44:28 +0800

    spec build extension, and dont build exists version

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -112,4 +112,81 @@ describe("CLI", function () {
         });
     });
 
+    it("run bin/main extension-dir, spec output and extension, gen dist files", function (done) {
+        var spawn = require('child_process').spawn;
+        var exec = spawn('node', [
+            path.resolve(__dirname, '..', 'bin', 'main'),
+            path.resolve(__dirname, 'target'),
+            '-o',
+            path.resolve(__dirname, 'target-dist'),
+            'mip-test1',
+        ]);
+
+        exec.on('close', function (code) {
+            var mainModule = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'mip-test1.js');
+            var utilModule = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'util.js');
+            var mainCSS = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'mip-test1.css');
+
+            expect(fileExists(mainModule)).toBeTruthy();
+            expect(fileExists(utilModule)).toBeTruthy();
+            expect(fileExists(mainCSS)).toBeTruthy();
+
+            var mainModuleContent = fs.readFileSync(mainModule, 'UTF-8');
+            var utilModuleContent = fs.readFileSync(utilModule, 'UTF-8');
+            var mainCSSContent = fs.readFileSync(mainCSS, 'UTF-8');
+
+            expect(mainModuleContent).toMatch(genDefinePattern('mip-test1', ['mip-test1/mip-test1']));
+            expect(mainModuleContent).toMatch(genDefinePattern('mip-test1/mip-test1', ['require', './util']));
+            expect(mainModuleContent).toMatch(genDefinePattern('mip-test1/util', []));
+
+            expect(utilModuleContent).toMatch(genDefinePattern('mip-test1/util', []));
+            expect(mainCSSContent).toMatch(/^mip-test1\{background:red/);
+
+
+            mainModule = path.resolve(__dirname, 'target-dist', 'mip-test2', '1.1.0', 'main.js');
+            var extModule = path.resolve(__dirname, 'target-dist', 'mip-test2', '1.1.0', 'ext.js');
+            mainCSS = path.resolve(__dirname, 'target-dist', 'mip-test2', '1.1.0', 'main.css');
+
+            expect(fileExists(mainModule)).toBeFalsy();
+            expect(fileExists(extModule)).toBeFalsy();
+            expect(fileExists(mainCSS)).toBeFalsy();
+
+            rmdir(path.resolve(__dirname, 'target-dist'));
+            done();
+        });
+    });
+
+    it("run bin/main extension-dir, spec output and extension, gen dist files", function (done) {
+        fs.mkdirSync(path.resolve(__dirname, 'target-dist'));
+        fs.mkdirSync(path.resolve(__dirname, 'target-dist', 'mip-test1'));
+        fs.mkdirSync(path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0'));
+
+        var spawn = require('child_process').spawn;
+        var exec = spawn('node', [
+            path.resolve(__dirname, '..', 'bin', 'main'),
+            path.resolve(__dirname, 'target'),
+            '-o',
+            path.resolve(__dirname, 'target-dist'),
+            'mip-test1',
+        ]);
+
+        var datas = [];
+        exec.stdout.on('data', function (data) {
+            datas.push(data.toString());
+        });
+
+        exec.on('close', function (code) {
+            var mainModule = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'mip-test1.js');
+            var utilModule = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'util.js');
+            var mainCSS = path.resolve(__dirname, 'target-dist', 'mip-test1', '1.0.0', 'mip-test1.css');
+
+            expect(fileExists(mainModule)).toBeFalsy();
+            expect(fileExists(utilModule)).toBeFalsy();
+            expect(fileExists(mainCSS)).toBeFalsy();
+            expect(datas.join('').indexOf('Exists') > 0).toBeTruthy();
+
+            rmdir(path.resolve(__dirname, 'target-dist'));
+            done();
+        });
+    });
 });
